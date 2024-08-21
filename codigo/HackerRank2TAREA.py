f76ef5f59368b7fb088ef39b7020ade954e735aa
GustavoMarca-sis112 <gustavo.marca@ucb.edu.bo>
2024-08-21 12:08:26 -0400

    otra tarea hackerrank

--- codigo/HackerRank2TAREA.py
+++ codigo/HackerRank2TAREA.py
@@ -33,3 +33,9 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
+
+EJERCICIO5 
+if __name__ == '__main__':
+    n = int(input())
+    for i in range (n):
+        print(i+1, end= "")
